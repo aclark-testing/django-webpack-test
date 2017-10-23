aba9047c67f9ca5f8a80f12f2f16788429a022de
Alex Clark <aclark@aclark.net>
2017-10-23 16:28:40 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -25,15 +25,6 @@ module.exports = {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract("css-loader", "style-loader"),
         },
-        {
-            test: /\.(png|jpg)$/, loader: "file-loader?name=images/[name].[ext]"
-        },
-        {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"
-        },
-        {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
-        },
       ],
     },  
 };
