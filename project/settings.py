a66f25ab06bc4c04ce189db8bf1e873a1673ca4e
Alex Clark <aclark@aclark.net>
2017-10-23 15:45:53 -0400

    Update

--- project/settings.py
+++ project/settings.py
@@ -132,3 +132,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'project', 'app', 'assets'),
 )
+
+# No webpack settings needed, use defaults
+#
+# WEBPACK_LOADER = {
+#     'DEFAULT': {
+#         'CACHE': not DEBUG,
+#         'BUNDLE_DIR_NAME': 'webpack_bundles/', # must end with slash
+#         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+#         'POLL_INTERVAL': 0.1,
+#         'TIMEOUT': None,
+#         'IGNORE': ['.+\.hot-update.js', '.+\.map']
+#     }
+# }
