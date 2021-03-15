767b776eb7c02cba6466f3ded502fb8bdb022d41
Alex Clark <aclark@aclark.net>
2021-03-15 17:10:54 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,16 @@
-var BundleTracker = require("webpack-bundle-tracker");
-var ExtractTextPlugin = require("extract-text-webpack-plugin-updated");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    context: __dirname + "/project/app/static",
-    entry: "./app",
-    output: {
-        path: __dirname + "/project/app/static/webpack_bundles",
-        filename: "[name]-[hash].js"
-    },
-    plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
-        new ExtractTextPlugin("[name]-[hash].css"),
-        // http://getbootstrap.com/docs/4.0/getting-started/webpack/#importing-javascript
-        new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
-          'window.jQuery': 'jquery',
-          Popper: ['popper.js', 'default'],
-        })
-    ],
-    // https://github.com/webpack-contrib/extract-text-webpack-plugin#usage
-    module: {
-      loaders: [
-        {   
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: "css-loader"
-            })
-        },
-      ],
-    },  
-};
+  context: __dirname,
+  entry: './project/app/static/js/index',
+  output: {
+      path: path.resolve('./assets/webpack_bundles/'),
+      filename: "[name]-[hash].js"
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
+}
